6100e3af0965d2a0a72b5204b633e0c3e31e2646
Dimpal Goyal <dimpalgoyal129@gmail.com>
2026-06-03 18:01:52 +0000

    started building dg-portfolio

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="p-4 flex justify-center border m-5 rounded-4xl items-center text-2xl">
+      portfolio
+    </div>
+    </>
+  )
+}
+
+export default App
